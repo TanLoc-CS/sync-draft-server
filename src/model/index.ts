9bb831d83ca8f516ec0a3e0bb29e952bab16be35
Tan Loc Nguyen <talu.dev@gmail.com>
2024-11-07 05:56:17 +1100

    added models

---
+++ src/model/index.ts
@@ -0,0 +1,5 @@
+import { UserModel, User } from "./user";
+import { DocumentModel, Document } from "./document";
+import { MergeModel, Merge } from "./merge";
+
+export { UserModel, User, DocumentModel, Document, MergeModel, Merge };
